042b349162da86f129dc624c3e81b4f85148ab5b
Christopher Taylor <chris.taylor@soundcloud.com>
2019-01-14 10:19:45 +0100

    check for POST method

--- runtime/src/main/scala/com/soundcloud/twinagle/Server.scala
+++ runtime/src/main/scala/com/soundcloud/twinagle/Server.scala
@@ -1,19 +1,18 @@
 package com.soundcloud.twinagle
 
 import com.twitter.finagle.Service
-import com.twitter.finagle.http.{MediaType, Request, Response}
+import com.twitter.finagle.http.{MediaType, Method, Request, Response}
 import com.twitter.util.Future
 
 
 /**
-  * A Twirp-compatible HTTP server/router. Takes a path-to-service mapping for all RPC methods.
+  * A Twirp-compatible HTTP server/router.
   * TwinagleExceptions are returned as conformant JSON responses, other exceptions are mapped
   * to twirp internal errors.
   *
   * TODO:
   * - extensibility/observability (metrics, logging, tracing, exception tracking, etc)
   * - do (de-)serialization errors need special handling?
-  * - check for POST method
   *
   * @param endpoints list of endpoints to expose
   */
@@ -23,18 +22,28 @@ class Server(endpoints: Seq[Endpoint]) extends Service[Request, Response] {
     acc + (ep.path -> ep.service)
   }
 
-  override def apply(request: Request): Future[Response] = services.get(request.path) match {
-    case Some(service) => service(request).handle {
-      case e: TwinagleException => errorResponse(e)
-      case e => errorResponse(new TwinagleException(e))
-    }
-    case None => Future.value(
-      errorResponse(TwinagleException(
-        ErrorCode.BadRoute,
-        s"unknown path: ${request.path}"
-      ))
-    )
+  override def apply(request: Request): Future[Response] = request.method match {
+    case Method.Post =>
+      services.get(request.path) match {
+        case Some(service) => service(request).handle {
+          case e: TwinagleException => errorResponse(e)
+          case e => errorResponse(new TwinagleException(e))
+        }
+        case None => Future.value(
+          errorResponse(TwinagleException(
+            ErrorCode.BadRoute,
+            s"unknown path: ${request.path}"
+          ))
+        )
 
+      }
+    case method =>
+      Future.value(
+        errorResponse(TwinagleException(
+          ErrorCode.BadRoute,
+          s"unsupported method $method (only POST is allowed)"
+        ))
+      )
   }
 
   private def errorResponse(twex: TwinagleException): Response = {
