4aa7d5ce2d9fac5389f5d73187710be7126a3e90
Christopher Taylor <chris.taylor@soundcloud.com>
2019-02-01 16:55:57 +0100

    fromResponse

--- runtime/src/main/scala/com/soundcloud/twinagle/JsonClientFilter.scala
+++ runtime/src/main/scala/com/soundcloud/twinagle/JsonClientFilter.scala
@@ -1,16 +1,27 @@
 package com.soundcloud.twinagle
 
-import com.twitter.finagle.http.{Request, Response}
+import com.twitter.finagle.http.{MediaType, Method, Request, Response}
 import com.twitter.finagle.{Filter, Service}
 import com.twitter.util.Future
+import scalapb.json4s.JsonFormat
 import scalapb.{GeneratedMessage, GeneratedMessageCompanion, Message}
 
 class JsonClientFilter[Req <: GeneratedMessage, Rep <: GeneratedMessage with Message[Rep] : GeneratedMessageCompanion](path: String) extends Filter[Req, Rep, Request, Response] {
-  private val converter = new JsonConverter
 
   override def apply(request: Req, service: Service[Request, Response]): Future[Rep] = {
-    val ev = implicitly[GeneratedMessageCompanion[Rep]]
-    val httpRequest = converter.mkRequest(path, request)
-    service(httpRequest).map(converter.fromResponse(_)(ev))
+    val httpRequest = serializeRequest(path, request)
+    service(httpRequest).map(deserializeResponse)
   }
+
+  def serializeRequest(path: String, r: Req): Request = {
+    val request = Request(Method.Post, path)
+    request.contentType = MediaType.Json
+    request.contentString = JsonFormat.toJsonString(r)
+    request
+  }
+
+  def deserializeResponse(response: Response): Rep = {
+    JsonFormat.fromJsonString[Rep](response.contentString)
+  }
+
 }
