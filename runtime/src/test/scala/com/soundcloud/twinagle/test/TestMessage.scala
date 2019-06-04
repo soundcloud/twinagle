1af83056663316233683b210ee266b82236d04df
Christopher Taylor <chris.taylor@soundcloud.com>
2019-06-04 18:35:58 +0200

    fix incompatibility of generated test messages
    the scalapb version bump changed how generated messages
    look.

--- runtime/src/test/scala/com/soundcloud/twinagle/test/TestMessage.scala
+++ runtime/src/test/scala/com/soundcloud/twinagle/test/TestMessage.scala
@@ -48,7 +48,7 @@ object TestMessage extends scalapb.GeneratedMessageCompanion[TestMessage] {
   def javaDescriptor: _root_.com.google.protobuf.Descriptors.Descriptor = TestProto.javaDescriptor.getMessageTypes.get(0)
   def scalaDescriptor: _root_.scalapb.descriptors.Descriptor = TestProto.scalaDescriptor.messages(0)
   def messageCompanionForFieldNumber(__number: _root_.scala.Int): _root_.scalapb.GeneratedMessageCompanion[_] = throw new MatchError(__number)
-  lazy val nestedMessagesCompanions: Seq[_root_.scalapb.GeneratedMessageCompanion[_]] = Seq.empty
+  lazy val nestedMessagesCompanions: Seq[_root_.scalapb.GeneratedMessageCompanion[_ <: _root_.scalapb.GeneratedMessage]] = Seq.empty
   def enumCompanionForFieldNumber(__fieldNumber: _root_.scala.Int): _root_.scalapb.GeneratedEnumCompanion[_] = throw new MatchError(__fieldNumber)
   lazy val defaultInstance = test.TestMessage(
   )
--- runtime/src/test/scala/com/soundcloud/twinagle/test/TestProto.scala
+++ runtime/src/test/scala/com/soundcloud/twinagle/test/TestProto.scala
@@ -8,7 +8,7 @@ package com.soundcloud.twinagle.test
 object TestProto extends _root_.scalapb.GeneratedFileObject {
   lazy val dependencies: Seq[_root_.scalapb.GeneratedFileObject] = Seq(
   )
-  lazy val messagesCompanions: Seq[_root_.scalapb.GeneratedMessageCompanion[_]] = Seq(
+  lazy val messagesCompanions: Seq[_root_.scalapb.GeneratedMessageCompanion[_ <: _root_.scalapb.GeneratedMessage]] = Seq(
     com.soundcloud.twinagle.test.TestMessage
   )
   private lazy val ProtoBytes: Array[Byte] =
